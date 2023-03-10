67541025f249d3046bfb8bd3315624b721e7939c
Christian Gil <christian@chrisgvdev.com>
2023-03-09 18:54:44 -0600

    client: Add scroll effect to the GlobalHeader

--- client/src/components/global/GlobalHeader.vue
+++ client/src/components/global/GlobalHeader.vue
@@ -9,10 +9,28 @@ const navigation = [
   { title: 'Services', route: '#services' },
   { title: 'Contact', route: '#contact' },
 ];
+
+/** SCROLL EFFECT FUNCTIONALITY */
+
+const wrapper = ref<HTMLElement | null>(null);
+const { height: wHeight } = useElementBounding(wrapper);
+const { y: scrollPos } = useWindowScroll();
+
+const isScrolled = computed(() => {
+  if (!wrapper.value) return false;
+  if (scrollPos.value <= wHeight.value / 2) return false;
+  return true;
+});
 </script>
 
 <template>
-  <header class="fixed inset-x-0 top-0 z-30">
+  <header
+    ref="wrapper"
+    :class="[
+      'fixed inset-x-0 top-0 z-30 bg-transparent transition-all duration-500',
+      isScrolled && 'bg-gray-900/80 backdrop-blur backdrop-saturate-150 backdrop-filter',
+    ]"
+  >
     <BaseContainer no-y-padding class="flex items-center justify-between py-4 sm:py-6">
       <NuxtLink to="/" class="default-rings rounded-md focus:ring-offset-gray-900">
         <img src="/mtec-logo.webp" alt="MTec Logo" class="h-12 sm:h-16" />
