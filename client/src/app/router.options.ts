02e2be07df151b0b2dfb2c466e39f5c17c2d0966
Christian Gil <christian@chrisgvdev.com>
2023-03-09 19:11:42 -0600

    client: Minor router behavior improvement

--- client/src/app/router.options.ts
+++ client/src/app/router.options.ts
@@ -3,7 +3,7 @@ import type { RouterConfig } from '@nuxt/schema';
 export default <RouterConfig>{
   scrollBehavior(to, from, savedPos) {
     if (savedPos) return savedPos;
-    if (to.fullPath === from.fullPath) return { top: 0, behavior: 'smooth' };
+    if (to.name === from.name && !to.hash) return { top: 0, behavior: 'smooth' };
     if (to.hash) return { el: to.hash, behavior: 'smooth', top: 120 };
     return { top: 0 };
   },
