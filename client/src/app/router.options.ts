a9b3af0d935cf971fc0d2e60443298a30d44dfbb
Christian Gil <christian@chrisgvdev.com>
2023-03-13 14:20:19 -0600

    client: Fix the scrollBehavior

--- client/src/app/router.options.ts
+++ client/src/app/router.options.ts
@@ -1,10 +1,75 @@
 import type { RouterConfig } from '@nuxt/schema';
+import type { RouteLocationNormalized, RouterScrollBehavior } from 'vue-router';
+// @ts-ignore
+import { appPageTransition as defaultPageTransition } from '#build/nuxt.config.mjs';
+
+type ScrollPosition = Awaited<ReturnType<RouterScrollBehavior>>;
+const HEADER_HEIGHT = 80;
 
 export default <RouterConfig>{
-  scrollBehavior(to, from, savedPos) {
-    if (savedPos) return savedPos;
-    if (to.name === from.name && !to.hash) return { top: 0, behavior: 'smooth' };
-    if (to.hash) return { el: to.hash, behavior: 'smooth', top: 120 };
-    return { top: 0 };
+  scrollBehavior(to, from, savedPosition) {
+    const nuxtApp = useNuxtApp();
+
+    // By default when the returned position is falsy or an empty object, vue-router will retain the current scroll position
+    // savedPosition is only available for popstate navigations (back button)
+    let position: ScrollPosition = savedPosition || undefined;
+
+    // Scroll to top if route is changed by default
+    if (!position && from && to && to.meta.scrollToTop !== false && _isDifferentRoute(from, to)) {
+      console.log('Scroll to top from default change');
+      position = { left: 0, top: 0 };
+    }
+
+    // Hash routes on the same page, no page hook is fired so resolve here
+    if (to.path === from.path) {
+      if (from.hash && !to.hash) {
+        console.log('Returning from hash reset');
+        return { left: 0, top: 0 };
+      }
+
+      if (to.hash) {
+        console.log('Returning hash element');
+        return { el: to.hash, top: _getHashElementScrollMarginTop(to.hash) };
+      }
+    }
+
+    // Wait for `page:transition:finish` or `page:finish` depending on if transitions are enabled or not
+    const hasTransition = (route: RouteLocationNormalized) =>
+      !!(route.meta.pageTransition ?? defaultPageTransition);
+    const hookToWait = hasTransition(from) && hasTransition(to) ? 'page:transition:finish' : 'page:finish';
+    return new Promise((resolve) => {
+      nuxtApp.hooks.hookOnce(hookToWait, async () => {
+        await nextTick();
+        if (to.hash) {
+          console.log('Return from other to.hash');
+          position = { el: to.hash, top: _getHashElementScrollMarginTop(to.hash) };
+        } else {
+          console.log('Return default', position);
+        }
+        resolve(position);
+      });
+    });
   },
 };
+
+function _getHashElementScrollMarginTop(selector: string): number {
+  try {
+    const elem = document.querySelector(selector);
+    if (elem) {
+      // Get element's scrollMarginTop or use default 80 pixels from the header
+      return parseFloat(getComputedStyle(elem).scrollMarginTop) || HEADER_HEIGHT;
+    }
+  } catch {}
+  return 0;
+}
+
+function _isDifferentRoute(a: RouteLocationNormalized, b: RouteLocationNormalized): boolean {
+  const samePageComponent = a.matched[0] === b.matched[0];
+  if (!samePageComponent) {
+    return true;
+  }
+  if (samePageComponent && JSON.stringify(a.params) !== JSON.stringify(b.params)) {
+    return true;
+  }
+  return false;
+}
