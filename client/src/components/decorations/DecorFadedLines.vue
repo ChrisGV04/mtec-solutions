a99ce2eedef9d54fe089ff13da431330cacdda84
Christian Gil <christian@chrisgvdev.com>
2023-03-13 15:18:15 -0600

    client: Implement the about us section

---
+++ client/src/components/decorations/DecorFadedLines.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div aria-hidden="true" class="flex flex-col items-end space-y-10 text-primary-600">
+    <svg xmlns="http://www.w3.org/2000/svg" view-box="672 1 0 0" class="h-px w-full" fill="none">
+      <path fill="url(#a)" fill-rule="evenodd" d="M0 0h672v1H0V0Z" clip-rule="evenodd" />
+      <defs>
+        <linearGradient id="a" x1="228.071" x2="0" y1="1.006" y2="1.006" gradientUnits="userSpaceOnUse">
+          <stop stop-color="currentColor" />
+          <stop offset="1" stop-color="currentColor" stop-opacity="0" />
+        </linearGradient>
+      </defs>
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" view-box="672 1 0 0" class="h-px w-3/4" fill="none">
+      <path fill="url(#a)" fill-rule="evenodd" d="M0 0h672v1H0V0Z" clip-rule="evenodd" />
+      <defs>
+        <linearGradient id="a" x1="228.071" x2="0" y1="1.006" y2="1.006" gradientUnits="userSpaceOnUse">
+          <stop stop-color="currentColor" />
+          <stop offset="1" stop-color="currentColor" stop-opacity="0" />
+        </linearGradient>
+      </defs>
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" view-box="672 1 0 0" class="h-px w-2/5" fill="none">
+      <path fill="url(#a)" fill-rule="evenodd" d="M0 0h672v1H0V0Z" clip-rule="evenodd" />
+      <defs>
+        <linearGradient id="a" x1="228.071" x2="0" y1="1.006" y2="1.006" gradientUnits="userSpaceOnUse">
+          <stop stop-color="currentColor" />
+          <stop offset="1" stop-color="currentColor" stop-opacity="0" />
+        </linearGradient>
+      </defs>
+    </svg>
+  </div>
+</template>
--- client/src/components/home/HomeAboutSection.vue
+++ client/src/components/home/HomeAboutSection.vue
@@ -1,7 +1,45 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section id="about">
-    <BaseContainer>About us</BaseContainer>
+  <section id="about" class="relative z-0 overflow-hidden">
+    <BaseContainer class="grid gap-10 md:grid-cols-2 lg:gap-20">
+      <div>
+        <span class="subtitle text-primary-700">About us</span>
+        <h2
+          class="mt-1 max-w-md font-display text-3xl font-semibold text-gray-900 sm:text-4xl md:max-w-none xl:text-5xl"
+        >
+          World Class Electronics Remanufacturing Services
+        </h2>
+
+        <div class="mt-8 space-y-4 text-gray-900 md:text-lg">
+          <p>
+            M Tec Solutions LLC is a passionate, flexible, fast paced and committed organization that provides
+            world class electronics remanufacturing services to our customers.
+          </p>
+
+          <p>
+            Our extensive electronics remanufacturing knowledge coupled with our understanding of diverse
+            markets makes us the ideal partner to meet all your aftermarket electronics needs.
+          </p>
+
+          <p>
+            We strive to be recognized within the Electronic OEM's (Original Equipment Manufacturer) as one of
+            the most reliable service providers for our customers remanufacturing needs in Latin America and
+            the United States.
+          </p>
+        </div>
+      </div>
+
+      <img
+        alt=""
+        class="w-full rounded object-cover object-center md:h-[80vh] md:min-h-full"
+        src="https://images.pexels.com/photos/9241760/pexels-photo-9241760.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+      />
+    </BaseContainer>
+
+    <DecorFadedLines class="absolute top-0 right-0 -z-10 w-[42rem] max-w-full opacity-30" />
+    <DecorFadedLines
+      class="absolute bottom-0 left-0 -z-10 w-[42rem] max-w-full rotate-180 transform opacity-30"
+    />
   </section>
 </template>
