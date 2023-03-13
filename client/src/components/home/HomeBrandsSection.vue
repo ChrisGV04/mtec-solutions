3b43cc955cbfb659d1440c0913466c9739bb38f6
Christian Gil <christian@chrisgvdev.com>
2023-03-13 14:39:07 -0600

    client: Implement the HomeBrandsSection

--- client/src/components/home/HomeBrandsSection.vue
+++ client/src/components/home/HomeBrandsSection.vue
@@ -2,6 +2,13 @@
 
 <template>
   <section>
-    <BaseContainer>Brands</BaseContainer>
+    <BaseContainer no-y-padding class="grid grid-cols-2 gap-8 py-16 md:py-24 lg:grid-cols-5 lg:gap-20">
+      <!-- TODO: Use marquee on mobile -->
+      <img src="/temp/cisco.png" class="h-12 w-full object-contain opacity-50" alt="Cisco" />
+      <img src="/temp/cisco.png" class="h-12 w-full object-contain opacity-50" alt="Cisco" />
+      <img src="/temp/cisco.png" class="h-12 w-full object-contain opacity-50" alt="Cisco" />
+      <img src="/temp/cisco.png" class="h-12 w-full object-contain opacity-50" alt="Cisco" />
+      <img src="/temp/cisco.png" class="h-12 w-full object-contain opacity-50" alt="Cisco" />
+    </BaseContainer>
   </section>
 </template>
