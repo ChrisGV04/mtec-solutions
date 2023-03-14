b3f5c4443496c2967a0995d5c24f6dccee324120
Christian Gil <christian@chrisgvdev.com>
2023-03-13 18:01:49 -0600

    client: Implement HomeVisionSection and improve the BaseContainer

--- client/src/components/base/BaseContainer.vue
+++ client/src/components/base/BaseContainer.vue
@@ -1,22 +1,30 @@
 <script setup lang="ts">
-import { PropType } from 'vue';
-
-defineProps({
+const props = defineProps({
   noYPadding: Boolean,
   noXPadding: Boolean,
   size: { type: String as PropType<'full' | 'sm' | 'xs'>, default: 'full' },
 });
+
+const styles = computed(() => {
+  let sizingClasses = '';
+
+  if (props.size === 'full') {
+    sizingClasses = 'max-w-7xl';
+    if (!props.noXPadding) sizingClasses += ' sm:px-6 lg:px-8 2xl:px-0';
+  } else if (props.size === 'sm') {
+    sizingClasses = 'max-w-3xl';
+    if (!props.noXPadding) sizingClasses += ' sm:px-6 lg:px-0';
+  } else {
+    sizingClasses = 'max-w-xl';
+    if (!props.noXPadding) sizingClasses += ' sm:px-0';
+  }
+
+  return sizingClasses;
+});
 </script>
 
 <template>
-  <div
-    class="mx-auto w-full"
-    :class="[
-      !noYPadding && 'py-24 lg:py-32',
-      !noXPadding && 'px-4 sm:px-6 lg:px-8',
-      size === 'full' ? 'max-w-7xl' : size === 'sm' ? 'max-w-3xl' : 'max-w-xl',
-    ]"
-  >
+  <div class="mx-auto w-full" :class="[!noYPadding && 'py-24 lg:py-32', !noXPadding && 'px-4', styles]">
     <slot />
   </div>
 </template>
--- client/src/components/home/HomeVisionSection.vue
+++ client/src/components/home/HomeVisionSection.vue
@@ -1,7 +1,27 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section>
-    <BaseContainer>Vision</BaseContainer>
+  <section class="relative z-0 overflow-hidden">
+    <BaseContainer size="xs" class="flex flex-col items-center text-center">
+      <span class="subtitle text-primary-50">Our vision</span>
+      <h2 class="mt-1 font-display text-3xl font-semibold text-white sm:text-4xl md:max-w-none xl:text-5xl">
+        Known for our Reliability, Flexibility and Adaptability
+      </h2>
+
+      <p class="mt-8 space-y-4 text-primary-50 md:text-lg">
+        M Tec Solutions LLC provides a competitive, high quality control and multiple engineering support
+        solution services; with a highly creative, motivated, and technically specialized work force that
+        drives continuous improvement throughout the organization, by constantly sensing customer changing
+        needs to jointly adapt to industry changes.
+      </p>
+    </BaseContainer>
+
+    <div class="absolute inset-0 -z-10 h-full w-full bg-primary-800 opacity-70" aria-hidden="true"></div>
+    <img
+      loading="lazy"
+      role="presentation"
+      class="absolute inset-0 -z-20 h-full w-full object-cover brightness-50 grayscale"
+      src="https://images.unsplash.com/photo-1581090466619-e945d2e2980e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c29sZGVyaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60"
+    />
   </section>
 </template>
